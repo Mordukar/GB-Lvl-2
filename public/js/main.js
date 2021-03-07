cceabcb49ac23d608a8fa466445b7544a385164a
Mordukar <t80_01@mail.ru>
2021-03-07 13:46:25 +0300

    Fix and optimaze

--- public/js/main.js
+++ public/js/main.js
@@ -1,50 +1,41 @@
-"use strict";
-
 class Validator {
-  constructor(formId) {
+  constructor(formId, config) {
+    this._form = null;
+    this._config = config;
     this.setListener(formId);
   }
 
   setListener(formId) {
-    const form = document.querySelector(formId);
+    this._form = document.querySelector(formId);
 
-    form.addEventListener("submit", (e) => {
+    this._form.addEventListener("submit", (e) => {
       e.preventDefault();
+      this._checkForm();
     });
   }
 
-  checkForm(inputs) {
-    const inputs = form.querySelectorAll("input");
+  _checkForm() {
+    const inputs = this._form.querySelectorAll("input");
 
-    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/gim;
-    const passwordRegex = /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/;
-    const telRegex = /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/;
+    Array.from(inputs)
+      .filter(({ name }) => this._config[name])
+      .forEach(({ name, value }) => {
+        const { validateRe } = this._config[name];
+        const isValid = validateRe.test(value);
 
-    Array.from(inputs).forEach((input) => {
-      const id = input.getAttribute("id");
-
-      let isValid = false;
-
-      switch (id) {
-        case "email":
-          isValid = emailRegex.test(input.value);
-          break;
-        case "password":
-          isValid = passwordRegex.test(input.value);
-          break;
-        case "tel":
-          isValid = telRegex.test(input.value);
-          break;
-        default:
-          break;
-      }
-
-      if (!isValid) {
-        console.log("Некорректно введено поле " + `${id}`);
-      }
-    });
+        if (!isValid) {
+          console.log(`Некорректно введено поле ${name}`);
+        }
+      });
   }
 }
 
-const formValidator = new Validator("#form");
-formValidator.checkForm();
+const formConfig = {
+  email: { validateRe: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/gim },
+  password: {
+    validateRe: /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/,
+  },
+  tel: { validateRe: /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/ },
+};
+
+new Validator("#form", formConfig);
